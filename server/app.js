5e204ccbeec3279190628efe4e45e063653b322e
nawap07 <pawanmahatoop07@gmail.com>
2024-12-15 20:51:17 +0530

    add

--- server/app.js
+++ server/app.js
@@ -1,45 +1,11 @@
-import express from "express";
-import http from "http"
-import { Server } from "socket.io";
+import express from "express"
 
-const PORT = 3000;
+const port = 3000;
 const app = express();
-
-const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:5173",
-        methods: ["GET", "POST"],
-        credentials: true
-    }
-})
-
-io.on("connection", (socket) => {
-    console.log(`User is connected - ${socket.id}`);
-
-    io.emit("msg", "Good Evening pawan mahato");
-
-    socket.on("message", ({ value, user }) => {
-        socket.emit("recive-message", value);
-        socket.to(user).emit("joinUser", value);
-    })
-
-    socket.on("join-room", (room) => {
-        socket.join(room)
-    })
-
-
-
-    socket.on("disconnect", () => {
-        console.log(`User Disconnected ID -${socket.id}`);
-    })
-
-})
-
 app.get("/", (req, res) => {
-    res.send("Good Evening pawan")
+    res.send("Good evening pawan ")
 })
 
-server.listen(PORT, () => {
-    console.log(`Server is started at PORT : http://localhost:${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is started at port : http://localhost:${port}`);
 })
